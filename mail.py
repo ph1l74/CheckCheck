a66f1be3034d26b5661afcd46e191f69fde00403
Filat Astakhov <ph1l74@mail.ru>
2017-06-22 17:20:23 +0500

    it's alive

--- mail.py
+++ mail.py
@@ -85,6 +85,6 @@ def send_mail(smtp_host, smtp_port, smtp_user, smtp_password,  smtp_sender, smtp
     mail.login(smtp_user, smtp_password)
 
     #sending mail and quit
-    mail.sendmail(smtp_sender, smtp_recievers, message.as_string())
+    mail.send_message(message)
     mail.quit()
 
