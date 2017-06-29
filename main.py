250c69801ab4d29c1d502066f4a8ce4f1597b4c0
Filat Astakhov <ph1l74@mail.ru>
2017-06-29 17:35:38 +0500

    mail.py works with batch of files main func in main totally reworked

--- main.py
+++ main.py
@@ -4,27 +4,34 @@ import ocr
 import db
 
 
-def check():
-    attachments = mail.get_attachments(config.imap)
+def check(count=1):
+
+    # get attachments from number of files
+    attachments = mail.get_attachments(config.imap, count)
     recognized_text = []
     if attachments:
-        print("Got some files")
         for attachment in attachments:
+            # get sender and batch of files
             sender = attachment[0][0]
             files =  attachment[1]
-            print(len(files))
+            print("Checking letter from {}. Number of attachments: {}".format(sender, len(files)))
             if files:
                 for file in files:
-                    text = ocr.image_to_text()
+                    text = ocr.image_to_text(file)
                     recognized_text.append(text)
             else:
                 mail.send_mail(config.smtp, sender, mail.messages['not found'])
-                if len(recognized_text) == len(files):
-                    for text in recognized_text:
-                        new_reciept = db.Reciept()
-                        new_reciept.id = text
-                        new_reciept.add()
-                else:
-                    mail.send_mail(config.smtp, sender, mail.messages['undefined'])
+                print("No files in mail. Email sent to {}". format(sender))
+
+            if len(recognized_text) == len(files):
+                for text in recognized_text:
+                    # new_reciept = db.Reciept()
+                    # new_reciept.id = text
+                    # new_reciept.add()
+                    print("Got some text:\n{}".format(text))
+            else:
+                mail.send_mail(config.smtp, sender, mail.messages['undefined'])
+    else:
+        print("No mails")
 
 check()
